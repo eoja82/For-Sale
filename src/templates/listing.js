17fdde4f578918618c18024e753093ea9238a809
Erik Oja <erik_oja@yahoo.com>
2020-09-16 20:21:31 -0500

    good start on img modal

--- src/templates/listing.js
+++ src/templates/listing.js
@@ -1,23 +1,49 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import { graphql } from "gatsby";
 import styles from "./listing.module.css"
 
+let imageIndex = 0
+
 export default ({ data }) => {
   const images = data.dataJson.images,
-        numImages = images.length
-  let imageIndex = 0
-  const [image, setImage] = useState(images[imageIndex])
+        numImages = images.length,
+        imgModal = useRef(null),
+        [image, setImage] = useState(images[imageIndex]),
+        [imgNumber, setImageNumber] = useState(imageIndex + 1)
   
-  function updateMainImage(e) {
-    let index = Number(e.target.dataset.imgIndex)
-    setImage(images[index])
+  function openImgModal() {
+    imgModal.current.style.display = "block"
   }
+  function closeImgModal() {
+    imgModal.current.style.display = "none"
+  }
+  function previousSlide() {
+    imageIndex--
+    if (imageIndex < 0) imageIndex = numImages - 1
+    setImage(images[imageIndex])
+    setImageNumber(imageIndex + 1)
+  }
+  function nextSlide() {
+    console.log("pre " + imageIndex)
+    imageIndex++
+    console.log(imageIndex)
+    if (imageIndex > numImages - 1) imageIndex = 0
+    console.log("imageIndex now " + imageIndex)
+    setImage(images[imageIndex])
+    setImageNumber(imageIndex + 1)
+  }
+  function updateMainImage(e) {
+    imageIndex = Number(e.target.dataset.imgIndex)
+    setImage(images[imageIndex])
+    setImageNumber(imageIndex + 1)
+  }
+
   return (
     <div>
       {/* <h1>{data.dataJson.title}</h1> */}
       <div className={styles.container}>
         <h1>{data.dataJson.title} - {data.dataJson.price}</h1>
-        <img className={styles.bigImage} src={image.src} alt={image.alt}></img>
+        <img className={styles.mainImage} src={image.src} alt={image.alt} onClick={openImgModal}></img>
         <div id={styles.thumbnailContainer}>
           {images.map( (x, i) => (
             <div className={styles.thumbnailDiv} key={`image${i}`}>
@@ -26,6 +52,18 @@ export default ({ data }) => {
           ))}
         </div>
           <p id={styles.description}>{data.dataJson.description}</p>
+          {/* modal images */}
+          <div id={styles.imgModalContainer} ref={imgModal}>
+            <span className={styles.closeModal} onClick={closeImgModal}>&times;</span>
+            <div className={styles.modalContent}>
+              <div className={styles.imgNumber}>
+                Image {imgNumber} of {numImages}
+              </div>
+              <img className={styles.modalImg} src={image.src} alt={image.alt}></img>
+              <a className={styles.previousImg} onClick={previousSlide}>&#10094;</a>
+              <a className={styles.nextImage} onClick={nextSlide}>&#10095;</a>
+            </div>
+          </div>
       </div>
     </div>
   )
