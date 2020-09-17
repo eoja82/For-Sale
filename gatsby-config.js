4eed2ed4c152e8dc67e8deecc75c711cf3b534bd
Erik Oja <erik_oja@yahoo.com>
2020-09-17 18:33:01 -0500

    update pathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@
 
 module.exports = {
   /* Your site config here */
-  /* pathPrefix: "/", */
+  pathPrefix: "/for-sale/",
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-json`,
