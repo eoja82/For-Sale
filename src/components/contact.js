c64698205ce140e477cdbd62178a6f8d1c720ac0
Erik Oja <erik_oja@yahoo.com>
2020-09-17 17:52:12 -0500

    initial commit

---
+++ src/components/contact.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import styles from "./contact.module.css"
+
+export default () => {
+  return (
+    <div>
+      <Helmet>
+        <html lang="en" />
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Reloading Equipment For Sale</title>
+        <meta name="description" content="Reloading equipment and books for sale. "  />
+        <meta name="keywords" content="reloading, books, scale, dies, press, vice" />
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous" />
+      </Helmet>
+        <div id={styles.contactContainer}>
+          <div>
+            <a className={styles.contactIcon} href="tel:1-555-555-5555"><i class="fa fa-phone"></i></a>
+            <a className={styles.contactIcon} href="sms:1-555-555-5555"><i class="fa fa-comment"></i></a>
+            <a className={styles.contact} href="tel:1-555-555-5555">555-555-5555</a>
+          </div>
+          <div>
+            <a className={styles.contactIcon} href="mailto:fake@email.com"><i class="fa fa-envelope"></i></a>
+            <a className={styles.contact} href="mailto:fake@email.com">fake@email.com</a>
+          </div>
+        </div>
+    </div>
+  )
+}
