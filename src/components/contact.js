b19739a9abdaab7d477fbb48d12247e455208001
Erik Oja <erik_oja@yahoo.com>
2020-09-18 13:40:15 -0500

    change title

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,7 +10,7 @@ export default () => {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <title>Reloading Equipment For Sale</title>
+        <title>My Stuff For Sale</title>
         <meta name="description" content="Reloading equipment and books for sale. "  />
         <meta name="keywords" content="reloading, books, scale, dies, press, vice" />
         <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous" />
