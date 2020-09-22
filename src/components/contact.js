38f87f580e86eff6503939e93f2522f2485124d2
Erik Oja <erik_oja@yahoo.com>
2020-09-22 16:35:01 -0500

    class to className on icons

--- src/components/contact.js
+++ src/components/contact.js
@@ -17,12 +17,12 @@ export default () => {
       </Helmet>
         <div id={styles.contactContainer}>
           <div>
-            <a className={styles.contactIcon} href="tel:1-555-555-5555" aria-label="Phone Number"><i class="fa fa-phone"></i></a>
-            <a className={styles.contactIcon} href="sms:1-555-555-5555" aria-label="Text Number"><i class="fa fa-comment"></i></a>
+            <a className={styles.contactIcon} href="tel:1-555-555-5555" aria-label="Phone Number"><i className="fa fa-phone"></i></a>
+            <a className={styles.contactIcon} href="sms:1-555-555-5555" aria-label="Text Number"><i className="fa fa-comment"></i></a>
             <a className={styles.contact} href="tel:1-555-555-5555">555-555-5555</a>
           </div>
           <div>
-            <a className={styles.contactIcon} href="mailto:fake@email.com" aria-label="Email"><i class="fa fa-envelope"></i></a>
+            <a className={styles.contactIcon} href="mailto:fake@email.com" aria-label="Email"><i className="fa fa-envelope"></i></a>
             <a className={styles.contact} href="mailto:fake@email.com">fake@email.com</a>
           </div>
         </div>
