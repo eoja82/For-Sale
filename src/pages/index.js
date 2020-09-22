aafce588f1067b9591efd214ffd91aab693955f4
Erik Oja <erik_oja@yahoo.com>
2020-09-21 19:04:58 -0500

    add Helmet info

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import styles from "./styles/index.module.css"
 import { useStaticQuery, graphql, Link, withPrefix } from "gatsby"
+import { Helmet } from "react-helmet"
 
 let Listings = () => {
   const data = useStaticQuery(graphql`
@@ -40,6 +41,15 @@ class Index extends React.Component {
   render() {
     return (
       <div>
+        <Helmet>
+          <html lang="en" />
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+          <title>My Stuff For Sale</title>
+          <meta name="description" content="My stuff for sale. "  />
+          <meta name="keywords" content="stuff, sale,, sell, buy, deals" />
+        </Helmet>
         <h1 id={styles.header}>My Stuff For Sale</h1>
         <Listings />
       </div>
