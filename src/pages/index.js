ab359d8c09d0678220ac64059d2269b48e0f553c
Erik Oja <erik_oja@yahoo.com>
2022-12-09 16:58:21 -0600

    rafactor to use Layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,15 @@
 import React from "react"
-import * as styles from "./styles/index.module.css"
-import { Helmet } from "react-helmet"
+import Layout from "../components/layout"
 import AllListings from "../components/allListings"
 
 
-class Index extends React.Component {
-  render() {
-    return (
-      <div>
-        <Helmet>
-          <html lang="en" />
-          <meta charset="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-          <title>My Stuff For Sale</title>
-          <meta name="description" content="My stuff for sale. "  />
-          <meta name="keywords" content="stuff, sale,, sell, buy, deals" />
-        </Helmet>
-        <h1 id={styles.header}>My Stuff For Sale</h1>
-        <AllListings />
-      </div>
-    )
-  }
+const Index = () => {
+  return (
+    <Layout>
+      <h1 className="text-center mt-4 mb-4 fw-bold">My Stuff For Sale</h1>
+      <AllListings />
+    </Layout>
+  )
 }
 
 export default Index
