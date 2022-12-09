2cc2edb087dd54424cbed17c6c369c08d5ce3d73
Erik Oja <erik_oja@yahoo.com>
2022-12-09 16:28:31 -0600

    refactor out Listings component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,7 @@
 import React from "react"
 import * as styles from "./styles/index.module.css"
-import { useStaticQuery, graphql, Link, withPrefix } from "gatsby"
 import { Helmet } from "react-helmet"
-
-let Listings = () => {
-  const data = useStaticQuery(graphql`
-    query SlugToQuery {
-      allDataJson {
-        nodes {
-          slug
-          images {
-            alt
-            src
-          }
-          description
-          price
-          title
-        }
-      }
-    }
-  `)
-  
-  let listings = data.allDataJson.nodes
-  return (
-    <div id={styles.listings}>
-      {listings.map ( (x, i) => (
-        <div className={styles.listing} key={`${x.slug}_${i}`}>
-          <Link to={x.slug} key={x.slug} className={styles.listingLink}>
-            {<img className={styles.thumbnail} src={withPrefix(x.images[0].src)} alt={x.images[0].alt} />}
-            <h3 className={styles.listingTitle}>{x.title} {x.price}</h3>
-          </Link>
-        </div>
-      ))}
-    </div>
-  )
-}
+import AllListings from "../components/allListings"
 
 
 class Index extends React.Component {
@@ -51,7 +18,7 @@ class Index extends React.Component {
           <meta name="keywords" content="stuff, sale,, sell, buy, deals" />
         </Helmet>
         <h1 id={styles.header}>My Stuff For Sale</h1>
-        <Listings />
+        <AllListings />
       </div>
     )
   }
