efb8337c682fce332f79d4fc9376e0b4511a1bc1
Erik Oja <erik_oja@yahoo.com>
2022-12-09 16:31:07 -0600

    update styles import for new styles directory

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import * as styles from "./contact.module.css"
+import * as styles from "./styles/contact.module.css"
 
 const Contact = () => {
   return (
