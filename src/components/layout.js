b0d23b618b182c97657b362e520250188c5e5ee9
Erik Oja <erik_oja@yahoo.com>
2022-12-10 18:09:47 -0600

    add font awesome linke

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,13 +12,19 @@ const Layout = ({ children }) => {
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <title>My Stuff For Sale</title>
         <meta name="description" content="My stuff for sale. "  />
-        <meta name="keywords" content="stuff, sale,, sell, buy, deals" />
+        <meta name="keywords" content="stuff, sale, sell, buy, deals" />
         {/* <link rel="icon" type="image/png" href="favicon.ico" /> */}
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
           crossorigin="anonymous"
+        />
+        <link 
+          href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" 
+          rel="stylesheet" 
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" 
+          crossorigin="anonymous" 
         />
       </Helmet>
       <Container fluid="true">
